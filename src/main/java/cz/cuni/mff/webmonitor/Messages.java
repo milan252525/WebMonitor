25acdfc8f54c5ea06ff0d5fc117f804a35a49cbc
milan252525 <milan.abraham2@seznam.cz>
2022-01-31 16:13:03 +0100

    separated messages from code

---
+++ src/main/java/cz/cuni/mff/webmonitor/Messages.java
@@ -0,0 +1,7 @@
+package cz.cuni.mff.webmonitor;
+
+import java.util.ResourceBundle;
+
+public class Messages {
+    public static final ResourceBundle messages = ResourceBundle.getBundle("messages");
+}
