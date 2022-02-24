83ae17e37db468bdcc80ea01fc581c6884b784c5
milan252525 <milan.abraham2@seznam.cz>
2022-02-24 17:06:00 +0100

    updated javadoc

--- docs/javadoc/package-search-index.js
+++ docs/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"cz.cuni.mff.webmonitor"},{"l":"cz.cuni.mff.webmonitor.config"},{"l":"cz.cuni.mff.webmonitor.notifications"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"cz.cuni.mff.webmonitor"},{"l":"cz.cuni.mff.webmonitor.config"},{"l":"cz.cuni.mff.webmonitor.monitoring"},{"l":"cz.cuni.mff.webmonitor.notifications"}];updateSearchResults();
